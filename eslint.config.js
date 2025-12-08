c827ec96e9143d8e1b3a834acafb29b77e82a3bd
darshilkadiwala <88822877+darshilkadiwala@users.noreply.github.com>
2025-12-09 00:24:49 +0530

    chore: update ESLint configuration and dependencies
    Enhance ESLint setup with improved configurations and additional plugins
    
    - Refactor `eslint.config.js` to include TypeScript support and better organization
    - Add new ESLint plugins for React and TypeScript linting
    - Update `package.json` to include new ESLint dependencies and versions
    - Modify `tsconfig.node.json` to include specific config file patterns
    - Clean up `tsconfig.json` by removing unnecessary compiler options

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,132 @@
+import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginReactRefresh from 'eslint-plugin-react-refresh';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import { configs as tslintConfigs, parser as tslintParser, plugin as tslintPlugin } from 'typescript-eslint';
+
+const compat = new FlatCompat({
+  baseDirectory: process.cwd(),
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
 
 export default defineConfig([
   globalIgnores(['dist']),
+  eslintConfigPrettier,
+  ...tslintConfigs.recommendedTypeChecked,
+  ...compat.extends('plugin:import/recommended', 'plugin:import/typescript', 'plugin:prettier/recommended'),
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    plugins: { '@typescript-eslint': tslintPlugin },
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: { ...globals.node, ...globals.jest },
+      parser: tslintParser,
+      ecmaVersion: 5,
+      parserOptions: {
+        project: ['tsconfig.app.json', 'tsconfig.node.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    settings: {
+      'import/resolver': { typescript: { project: './tsconfig.app.json' } },
+    },
+    rules: {
+      '@typescript-eslint/consistent-indexed-object-style': ['error', 'record'],
+
+      '@typescript-eslint/consistent-type-exports': ['error', { fixMixedExportsWithInlineTypeSpecifier: false }],
+
+      '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        { fixStyle: 'separate-type-imports', prefer: 'type-imports' },
+      ],
+
+      '@typescript-eslint/explicit-function-return-type': 'error',
+
+      // Disallow invalid uses of `this`
+      '@typescript-eslint/no-invalid-this': 'error',
+
+      // Enforce explicit `this` parameter type annotations
+      '@typescript-eslint/explicit-module-boundary-types': 'error',
+
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: ['parameter', 'variable'],
+          leadingUnderscore: 'require',
+          format: ['camelCase'],
+          modifiers: ['unused'],
+        },
+        {
+          selector: ['parameter', 'variable'],
+          leadingUnderscore: 'allowDouble',
+          format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+        },
+      ],
+
+      '@typescript-eslint/no-empty-object-type': ['error', { allowInterfaces: 'with-single-extends' }],
+
+      '@typescript-eslint/no-extraneous-class': [
+        'error',
+        { allowConstructorOnly: true, allowStaticOnly: true, allowWithDecorator: true },
+      ],
+
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+        },
+      ],
+
+      'no-restricted-exports': [
+        'error',
+        {
+          restrictDefaultExports: {
+            direct: true,
+            named: true,
+            defaultFrom: true,
+            namedFrom: true,
+            namespaceFrom: true,
+          },
+        },
+      ],
+    },
+  },
+  {
+    ...pluginReact.configs.flat.recommended,
+    languageOptions: {
+      ...pluginReact.configs.flat.recommended?.languageOptions,
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+    },
+  },
+  {
+    plugins: { 'react-hooks': pluginReactHooks, 'react-refresh': pluginReactRefresh },
+    settings: { react: { version: 'detect' } },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      // React scope no longer necessary with new JSX transform.
+      'react/react-in-jsx-scope': 'off',
+      // React Refresh
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+  },
+  {
+    files: ['*.config.?(m)js', '*.config.?(c)js', '*.config.?(m)ts'],
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+      'import/no-default-export': 'off',
+      'no-restricted-exports': 'off',
     },
   },
 ]);
